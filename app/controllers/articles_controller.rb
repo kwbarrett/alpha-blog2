4bfb2c1dbd1cdc23cca72385298d11c892677a15
Kenneth Barrett <kenneth.barrett.jr@gmail.com>
2019-12-13 23:48:32 -0500

    Add pagination to views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,18 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
-  
+
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
     @article = Article.new
   end
-  
+
   def edit
     # render plain: @article.inspect
   end
-  
+
   def create
     # debugger
     # render plain: params[:article].inspect
@@ -25,6 +25,7 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
   def update
     # render plain: params[:article].inspect
     if @article.update(article_params)
@@ -34,25 +35,25 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def show
     # render plain: @article.inspect
   end
-  
+
   def destroy
     @article.destroy
     flash[:danger] = 'Article deleted!!'
     redirect_to articles_path
   end
-  
+
   private
   def article_params
     params.require(:article).permit(:title, :description)
     # render plain: params[:article].inspect
   end
-  
+
   def set_article
     @article = Article.find(params[:id])
   end
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-  
+
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
   def new
     @user = User.new
   end
-  
+
   def create
     # render plain: params[:user].inspect
     @user = User.new(user_params)
@@ -17,11 +17,11 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -31,11 +31,12 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def show
     @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
-  
+
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
